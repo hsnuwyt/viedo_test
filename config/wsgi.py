f224cee2983843cf9f4c573fb589eb2d40a73e2d
WYT <hsnuwyt@gmail.com>
2021-04-21 02:21:50 +0800

    make it better

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from dj_static import Cling
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+
