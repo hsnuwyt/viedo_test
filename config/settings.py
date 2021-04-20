754fcc0cdda8b59954738d122cf0d3b1a8298b14
WYT <hsnuwyt@gmail.com>
2021-04-21 02:05:10 +0800

    make it better

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=hppio+lh(5=my_o$ul!*a0%_hr4*@j8!c&)0n8lufccn2lzh@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
